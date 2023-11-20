13dab823c642b0613a4e2d36c3c5df73e4b5b5b7
Samima Hassan <samimahassan55@gmail.com>
2023-11-20 19:36:07 +0100

    data for chart 2

---
+++ src/charts/chartData.tsx
@@ -0,0 +1,13 @@
+
+export type ChartData = {
+    country: string;
+    co2Emission2010: number;
+  };
+  
+  export const data: ChartData[] = [
+    { country: 'USA', co2Emission2010: 5421 },
+    { country: 'China', co2Emission2010: 9041 },
+    { country: 'India', co2Emission2010: 1820 },
+    { country: 'Canada', co2Emission2010: 4820 },
+  ];
+  
